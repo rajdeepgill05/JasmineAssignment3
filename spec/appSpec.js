ff4cc095adf3a59161e35c34cfecd254ec02495a
SD13 <SD13@mitt.ca>
2018-06-01 13:14:36 -0500

    Initial Commit

---
+++ spec/appSpec.js
@@ -0,0 +1,47 @@
+describe("App", () => {
+  describe("Counter", () => {
+    it("is set to 120 when the button is clicked", () => {
+
+    })
+  })
+
+  describe("Button", () => {
+    it("has an ID of 'click-me'", () => {
+
+    })
+  })
+
+  describe("Factorial", () => {
+    it("returns the factorial of positive numbers", () => {
+
+    })
+
+    it("returns 1 if the provided number is less than 1 ", () => {
+
+    })
+  })
+
+  describe("Computer", () => {
+    it("is created with a hard drive size of 512", () => {
+
+    })
+
+    describe("installProgram", () => {
+      it("can install a program if there is sufficient space", () => {
+
+      })
+
+      it("will not install the program if there is insufficient space", () => {
+
+      })
+    })
+
+    describe("format", () => {
+      it("resets the hard drive to 512, even after programs have been installed", () => {
+
+      });
+    })
+
+  })
+
+})
